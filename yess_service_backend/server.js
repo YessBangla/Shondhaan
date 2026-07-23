816f79ab25c4876b124148bf18ce2cc963aca5a4
NI-Tushar <nitushar06@gmail.com>
2026-07-23 16:39:53 +0600

    yess service backend data fetching issue solved

--- yess_service_backend/server.js
+++ yess_service_backend/server.js
@@ -32,7 +32,7 @@ const corsOrigin = [
         .filter(Boolean),
       process.env.FRONTEND_BASE_URL,
       "http://localhost:5173",
-      "https://shondhaan.yessbd.top",
+      "https://shondhaan.com",
       "https://www.shondhaan.com",
     ].filter(Boolean)
   ),
